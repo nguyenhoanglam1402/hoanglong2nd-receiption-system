7bcc7b9584065635b0e81f696757a944f21a8d28
nguyenhoanglam1402 <lamnhgcd191296@fpt.edu.vn>
2021-03-21 19:59:37 +0700

    Test #11

--- js/effect.js
+++ js/effect.js
@@ -2,7 +2,7 @@ function setStatusOfCustomerCard(){
     var customerItem = document.getElementsByClassName('customer-item-card');
     var dueDay = document.getElementsByClassName('date');
     for(var index = 0; index < customerItem.length; index++){
-        if(calculateDifferentDay(dueDay[index].value) <=5){
+        if(calculateDifferentDay(dueDay[index].innerHTML) <=5){
             customerItem[index].style.backgroundColor = '#fc891275';
         }
     }
@@ -19,5 +19,3 @@ function calculateDifferentDay(dateEnd){
     var days_difference = time_difference / (1000 * 60 * 60 * 24);  
     return days_difference;
 }
-
-document.addEventListener("DOMContentLoaded", setStatusOfCustomerCard);
