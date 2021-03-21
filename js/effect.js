3457f87af57a4b1d833073d932cc1861260ac323
nguyenhoanglam1402 <lamnhgcd191296@fpt.edu.vn>
2021-03-21 20:11:01 +0700

    Test #13

--- js/effect.js
+++ js/effect.js
@@ -6,7 +6,7 @@ function setStatusOfCustomerCard(){
         if( remainingDate >= 3 && remainingDate <=7){
             customerItem[index].style.backgroundColor = '#fc891275';
         }
-        else if(remainingDate <= 0){
+        else if(remainingDate < 3){
             customerItem[index].style.backgroundColor = '#fc1d1275';
         }
         else{
