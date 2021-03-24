606a73d49201d5ef70ce8f611bb6c2d7e92b1907
nguyenhoanglam1402 <lamnhgcd191296@fpt.edu.vn>
2021-03-24 23:14:18 +0700

    #30

--- index.php
+++ index.php
@@ -8,7 +8,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <script src="https://cdn.jsdelivr.net/npm/chart.js@2.8.0"></script>
     <!-- Bootstrap CSS -->
-    <link rel="shortcut icon" href="./images/image_for_page/bootstrap-logo.svg" type="image/svg">
+    <link rel="shortcut icon" href="./images/image_for_page/bootstrap-logo.svg" type="image/svg" size="96x96">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <link rel="stylesheet" href="css/style.css" >
   </head>
