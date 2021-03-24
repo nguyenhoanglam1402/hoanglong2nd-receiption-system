0b8d3bbba9be4bb86b69624b6e14849da06606be
nguyenhoanglam1402 <lamnhgcd191296@fpt.edu.vn>
2021-03-24 17:31:08 +0700

    Test 30

---
+++ js/receiption-form.js
@@ -0,0 +1,40 @@
+var customerName = document.getElementById('customer-name-input').value;
+var typeOfService = document.getElementById('type-service-selector').value;
+var description = document.getElementById('description-input').value;
+var totalMoney = document.getElementById('total-money-input').value;
+var deposit = document.getElementById('deposit-input').value;
+var dateTime = document.getElementById('datetime-input').value;
+var phoneNumber = document.getElementById('phonenumber').value;
+
+
+function cancel(){
+    location.href('../createForm.html');
+}
+
+function sendReceiption(){
+    if(isValidated){
+        var xhtml = new XMLHttpRequest();
+        xhtml.open("POST", "../server-side/submit_receiption.php", true);
+        xhtml.send();
+    }
+}
+
+function checkValidated(){
+    var isValidated = false;
+    if(customerName == ''){
+        alert('Bạn nên nhập tên khách hàng !');
+    } else if (description == ''){
+        alert('Bạn không nên bỏ trống chi tiết biên lai !');
+    } else if (totalMoney <= 0 || totalMoney == null){
+        alert('Tổng tiền của sản phẩm không hợp lệ !');
+    } else if (deposit <= 0 || deposit == null){
+        alert('Tiền đặt cọc không được dưới 0 đồng hoặc bỏ trống');
+    } else if (dateTime == '' || dateTime == null){
+        alert('Vui lòng định trước ngày hẹn của bạn với khách hàng !');
+    } else if (phoneNumber == 0 || phoneNumber == null){
+        alert('Vui lòng nhập số điện thoại của khách hàng để thuận tiện cho việc liên lạc!');
+    } else{
+        isValidated = true;
+    }
+    return isValidated;
+}
