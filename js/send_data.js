c74ac2be6f5a82f64889ffc50c739a9a01f7489f
nguyenhoanglam1402 <lamnhgcd191296@fpt.edu.vn>
2021-03-28 15:21:27 +0700

    #4

---
+++ js/send_data.js
@@ -0,0 +1,16 @@
+$(document).ready(function () {
+
+    $('#send-button').click(function (e) { 
+        
+    });
+
+    $('#cancel-button').click(function (e) { 
+        cancel();        
+    });
+
+    function cancel(){
+        window.location.href('/index.php');
+    }
+
+    function 
+});
