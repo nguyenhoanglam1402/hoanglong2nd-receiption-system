602f7cf5d3c1fd00eb2025aa36031149055d82a8
nguyenhoanglam1402 <lamnhgcd191296@fpt.edu.vn>
2021-03-29 11:45:09 +0700

    #4

--- server-side/checkAvailableUser.php
+++
@@ -1,6 +0,0 @@
-<?php
-require '/server-side/connect.php';
-pg_parameter_status($connect);
-$query = "select count(cus_id) from customer where customer_name = '?'";
-
-?>
---
+++ server-side/submit_receiption.php
@@ -0,0 +1,27 @@
+<?php
+//check connect with Server
+require 'connect.php';
+//parameters
+$name = $_POST['name'];
+$type = $_POST['type'];
+$descr = $_POST['descr'];
+$last = $_POST['last'];
+$depos = $_POST['depos'];
+$date = $_POST['date'];
+$phone = $_POST['phone'];
+$createdDate = date('Y/m/d');
+//check exist customer via their name and phone number
+$cusid = checkAvailableCustomer();
+//query 
+$query = "INSERT INTO receiption_product_service
+(cid, title, description, expected_delivery_date, created_date, deposit, rest_amount, is_done)
+values($1, $2, $3, $4, $5, $6, $7, false)";
+$result = pg_query_params($connect, $query, array($cusid, $type, $descr, $date, $createdDate, $depos, $last, $phone));
+//check 
+function checkAvailableCustomer(){
+    $checkingQuery = "SELECT cus_id FROM customer WHERE customer_name = $1 AND phone_number = $2";
+    $resultCheckingQuery = pg_query_params($connect, $query, array($name, $phone)); 
+    $row = pg_fetch_row($resultCheckingQuery, 0);
+    return $row;
+}
+?>
