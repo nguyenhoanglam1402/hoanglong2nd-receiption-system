8105641dd9154bb7bcb5da5f253f459430334aa3
nguyenhoanglam1402 <lamnhgcd191296@fpt.edu.vn>
2021-03-29 11:56:48 +0700

    #4

--- server-side/submit_receiption.php
+++ server-side/submit_receiption.php
@@ -11,10 +11,9 @@ $date = $_POST['date'];
 $phone = $_POST['phone'];
 $createdDate = date('Y/m/d');
 //check exist customer via their name and phone number
-$checkingQuery = "SELECT cus_id FROM customer WHERE customer_name = $1 AND phone_number = $2";
-$resultCheckingQuery = pg_query_params($connect, $query, array($name, $phone)); 
+$checkingQuery = "SELECT cus_id FROM customer WHERE customer_name = $name AND phone_number = $phone";
+$resultCheckingQuery = pg_query($connect, $query, array($name, $phone)); 
 $cusid= pg_fetch_row($resultCheckingQuery, 0);
 //query 
-
 //check 
 ?>
