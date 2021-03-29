45a4ffd524f506ef205e1165a1134d1311b0c24b
nguyenhoanglam1402 <lamnhgcd191296@fpt.edu.vn>
2021-03-29 19:11:03 +0700

    #4

--- server-side/submit_receiption.php
+++ server-side/submit_receiption.php
@@ -2,6 +2,7 @@
 //check connect with Server
 require 'connect.php';
 //parameters
+echo "<script>alert('Im here');</script>";
 $name = $_POST['name'];
 $type = $_POST['type'];
 $descr = $_POST['descr'];
@@ -15,9 +16,6 @@ echo "<script>alert('$name');</script>";
 $checkingQuery = "SELECT cus_id FROM customer WHERE customer_name = '$name' AND phone_number = '$phone'";
 $resultCheckingQuery = pg_query($connect, $checkingQuery);
 while($resultOfRows = pg_fetch_row($resultCheckingQuery)){
-    echo '<script>console.log("'.$resultOfRows[0].'");</script>';
+    echo "<script>console.log('".$resultOfRows[0]."');</script>";
 }
-//$cusId = $resultOfRows[0];
-//query 
-//check 
 ?>
