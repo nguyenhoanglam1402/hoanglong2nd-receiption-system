2889fa7415f8feeb32d6d337dae33eaa740ba283
nguyenhoanglam1402 <lamnhgcd191296@fpt.edu.vn>
2021-03-29 11:53:05 +0700

    #4

--- server-side/submit_receiption.php
+++ server-side/submit_receiption.php
@@ -11,13 +11,6 @@ $date = $_POST['date'];
 $phone = $_POST['phone'];
 $createdDate = date('Y/m/d');
 //check exist customer via their name and phone number
-$checkingQuery = "SELECT cus_id FROM customer WHERE customer_name = $1 AND phone_number = $2";
-$resultCheckingQuery = pg_query_params($connect, $query, array($name, $phone)); 
-$cusid= pg_fetch_row($resultCheckingQuery, 0);
-//query 
-$query = "INSERT INTO receiption_product_service
-(cid, title, description, expected_delivery_date, created_date, deposit, rest_amount, is_done)
-values($1, $2, $3, $4, $5, $6, $7, false)";
-$result = pg_query_params($connect, $query, array($cusid, $type, $descr, $date, $createdDate, $depos, $last, $phone));
+
 //check 
 ?>
