ecf826fcc0a292f023d328dcea4d31f7747742f6
nguyenhoanglam1402 <lamnhgcd191296@fpt.edu.vn>
2021-03-29 15:13:32 +0700

    #4

--- server-side/submit_receiption.php
+++ server-side/submit_receiption.php
@@ -11,11 +11,11 @@ $date = $_POST['date'];
 $phone = $_POST['phone'];
 $createdDate = date('Y/m/d');
 //check exist customer via their name and phone number
-$checkingQuery = "SELECT cus_id FROM customer WHERE customer_name = $name AND phone_number = $phone";
-$resultCheckingQuery = pg_query($connect, $query); 
+$checkingQuery = "SELECT cus_id FROM customer WHERE customer_name = '$name' AND phone_number = '$phone'";
+$resultCheckingQuery = pg_query($connect, $query);
+
 //$resultOfRows = pg_fetch_row($resultCheckingQuery);
 //$cusId = $resultOfRows[0];
-echo'alert("'.$resultCheckingQuery.'")';
 //query 
 //check 
 ?>
