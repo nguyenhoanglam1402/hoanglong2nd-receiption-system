0c4828b64eef6ee32453926a297a6abc730e7075
nguyenhoanglam1402 <lamnhgcd191296@fpt.edu.vn>
2021-04-04 03:32:16 +0700

    #100

--- server-side/submit_receiption.php
+++ server-side/submit_receiption.php
@@ -11,9 +11,10 @@ $date = $_POST['date'];
 $creatDate = $_POST['credate'];
 $phone = $_POST['phone'];
 //check exist customer via their name and phone number
-while(checkCustomerID() != true){
+while(checkCustomerID() != null){
     createCustomerInformation();
 }
+$customerID = checkCustomerID();
 createNewReceiption();
 
 
@@ -23,23 +24,24 @@ function checkCustomerID(){
     while($resultOfRows = pg_fetch_assoc($resultCheckingQuery)){
         if($resultOfRows['cus_id'] != null || $resultOfRows != ''){
             //submit to database
-            return true;
+            return $resultOfRows['cus_id'];
         }
         else{
             //create new customer
-            return false;
+            return null;
         }
     }
 }
 
 function createCustomerInformation(){
     $query = "INSERT INTO customer (customer_name, phone_number) VALUES ('$name', '$phone')";
+    pg_query($connect, $query);
 }
 
 function createNewReceiption(){
     $requestSubmitQuery = "INSERT INTO receiption_product_service 
     (cid, title, description, expected_delivery_date, created_date, deposit, rest_amount, is_done)
-    VALUES(".$resultOfRows['cus_id'].", '$type', '$descr', '$date', '$creatDate', $depos, $last, false)";
+    VALUES(".$customerID.", '$type', '$descr', '$date', '$creatDate', $depos, $last, false)";
     $resultOfCreationQuery = pg_query($connect, $requestSubmitQuery);
     echo 'Hóa đơn đã được xử lý !';
 }
