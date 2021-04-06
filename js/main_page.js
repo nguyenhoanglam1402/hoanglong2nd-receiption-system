cccc0d69fc09ff67a51e326c7ad072e82349f017
nguyenhoanglam1402 <lamnhgcd191296@fpt.edu.vn>
2021-04-07 03:03:39 +0700

    #125

--- js/main_page.js
+++ js/main_page.js
@@ -72,7 +72,7 @@ function drawChart(){
                     label: 'Ảnh cưới',
                     backgroundColor: '#fff0',
                     borderColor: '#007bff',
-                    data: datasets.getData()
+                    data: dataset.getData()
                 },
                 {
                     label: 'Thuê áo cưới',
