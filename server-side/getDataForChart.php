377ccee6834024b1d6ff163983d7f01b5b008dd4
nguyenhoanglam1402 <lamnhgcd191296@fpt.edu.vn>
2021-04-06 19:45:53 +0700

    #124

--- server-side/getDataForChart.php
+++ server-side/getDataForChart.php
@@ -5,9 +5,7 @@ include 'WeeklyData.php';
 
 $startDate = '2021-4-1';
 $endDate = '2021-4-14';
-
 $weeklyDate = new WeeklyData($startDate, $endDate, $connect);
-
 echo json_encode($weeklyDate);
 
 //fetch data from PostgreSQL
