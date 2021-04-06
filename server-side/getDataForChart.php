09178e3527273a4d3dd076a9eb29cd7223953071
nguyenhoanglam1402 <lamnhgcd191296@fpt.edu.vn>
2021-04-06 19:14:30 +0700

    #124

--- server-side/getDataForChart.php
+++ server-side/getDataForChart.php
@@ -6,7 +6,7 @@ include 'WeeklyData.php';
 $startDate = '2021-4-1';
 $endDate = '2021-4-14';
 
-function &fetchDailyData($startDate, $endDate, $connect): array
+function &fetchDailyData($startDate, $endDate, $connect)
 {
     $weeklyDate = new WeeklyData($startDate, $endDate, $connect);
     return $weeklyDate;
