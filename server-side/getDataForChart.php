98e634d4e130c80765f7a2b783517cb5ab1b6619
nguyenhoanglam1402 <lamnhgcd191296@fpt.edu.vn>
2021-04-07 02:16:45 +0700

    #125

--- server-side/getDataForChart.php
+++ server-side/getDataForChart.php
@@ -3,8 +3,8 @@
 require 'connect.php';
 include 'WeeklyData.php';
 
-$startDate = '2021-4-1';
-$endDate = '2021-4-14';
+$startDate = '2021-4-3';
+$endDate = '2021-4-10';
 $weeklyDate = new WeeklyData($startDate, $endDate, $connect);
 $weeklyDate->setDailyData($connect);
 
