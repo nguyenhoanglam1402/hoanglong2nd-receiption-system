b64c4967bc7a4e8c630151ec0ccc827d1e3309f9
nguyenhoanglam1402 <lamnhgcd191296@fpt.edu.vn>
2021-04-06 20:47:16 +0700

    #125

--- server-side/WeeklyData.php
+++ server-side/WeeklyData.php
@@ -17,9 +17,9 @@ class WeeklyData
     public function setDailyData($connect)
     {
         for($date = strtotime($this->dateStart); $date <= strtotime($this->dateEnd); $date = strtotime("+1 day", $date)){
-            $dailyData = new DailyData();
-            $dailyData->setNodes($date, $connect);
-            array_push($this->weeklyData, $dailyData);
+//            $dailyData = new DailyData();
+//            $dailyData->setNodes($date, $connect);
+//            array_push($this->weeklyData, $dailyData);
         }
     }
 }
