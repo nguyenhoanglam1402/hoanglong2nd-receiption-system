17d08cbbdcf4471c4e72f55c6caec83291c53c04
nguyenhoanglam1402 <lamnhgcd191296@fpt.edu.vn>
2021-04-06 19:00:07 +0700

    #123

--- server-side/getDataForChart.php
+++ server-side/getDataForChart.php
@@ -9,13 +9,13 @@ include 'WeeklyData.php';
 $startDate = '2021-4-1';
 $endDate = '2021-4-14';
 
-#[Pure] function fetchDailyData($startDate, $endDate, $connect): array
+function &fetchDailyData($startDate, $endDate, $connect): array
 {
     $weeklyDate = new WeeklyData($startDate, $endDate, $connect);
     return $weeklyDate->ExportWeeklyData();
 }
 
-$result = fetchDailyData($startDate, $endDate, $connect);
+$result =& fetchDailyData($startDate, $endDate, $connect);
 echo json_encode($result);
 
 //fetch data from PostgreSQL
