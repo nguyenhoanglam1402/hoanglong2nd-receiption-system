c5e3d41007af372bb6b0685b35e747ffcf56279d
nguyenhoanglam1402 <lamnhgcd191296@fpt.edu.vn>
2021-04-11 15:55:38 +0700

    Test Version

--- js/customer_item_process.js
+++ js/customer_item_process.js
@@ -1,5 +1,6 @@
 document.body.onclick = function(element){
     if(element.target.getAttribute("class") == "customer-item-card") {
         sessionStorage.setItem('receiption', element.target.getAttribute("id"));
+        console.log("I'm here ");
     }
 }
