86bddd4a210165c8fd988e105d1124ced1e56ef8
nguyenhoanglam1402 <lamnhgcd191296@fpt.edu.vn>
2021-05-07 13:55:45 +0700

    Modified font, color text, logo and receiption component

--- index.php
+++ index.php
@@ -4,13 +4,13 @@
     <title>Hoang Long II Kit</title>
     <!-- Required meta tags -->
     <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width; initial-scale=1.0;">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <script src="https://cdn.jsdelivr.net/npm/chart.js@2.8.0"></script>
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous"/>
     <link rel="shortcut icon" href="./images/image_for_page/download.png" type="image/png" sizes="96x96">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-    <link rel="stylesheet" href="css/style.css" >
+    <link rel="stylesheet" type="text/css" href="css/style.css" >
   </head>
   <body>
     <div class="navbar-session">
