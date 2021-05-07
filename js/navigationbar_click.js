aadc33f27d21f82fb99f9a4632595087a9112fd6
nguyenhoanglam1402 <lamnhgcd191296@fpt.edu.vn>
2021-05-08 00:53:52 +0700

    Modified font, color text, logo and receiption page #4

--- js/navigationbar_click.js
+++ js/navigationbar_click.js
@@ -1,6 +1,8 @@
 $(document).ready(function () {
     $(".nav-link").click(function (event) { 
         var link = $(this).attr("href");
+        $(".nav-link").removeClass("active");
+        $(this).addClass("active");
         $.ajax({
             url: link,
         })
