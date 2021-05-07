e5e9db3d37029789f430902f996302da21b8eb66
nguyenhoanglam1402 <lamnhgcd191296@fpt.edu.vn>
2021-05-07 13:53:30 +0700

    Modified font, color text, logo and receiption component

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
     <title>Hoang Long II Kit</title>
     <!-- Required meta tags -->
     <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="viewport" content="width=device-width; initial-scale=1.0;">
     <script src="https://cdn.jsdelivr.net/npm/chart.js@2.8.0"></script>
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous"/>
@@ -103,7 +103,10 @@
               </h1>
             </div>
             <ul class="listCustomer">
-	            <li class = "trending-list-item">Đang tải dữ liệu !</li>
+	            <li class = "trending-list-item">
+                <i class="fas fa-cloud-download-alt"></i>
+                Đang tải dữ liệu... 
+              </li>
             </ul>
           </div>
         </div>
