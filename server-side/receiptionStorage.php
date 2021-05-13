7c4583f6c741a78b9d6d8f7f2217a43101a1e0db
nguyenhoanglam1402 <nguyenhoanglam.emily@gmail.com>
2021-05-14 00:06:42 +0700

    Modified some task tab interface

--- server-side/receiptionStorage.php
+++ server-side/receiptionStorage.php
@@ -5,8 +5,8 @@ $function = $_POST['function'];
 $status = $_POST['updatedStatus'];
 $receiptionId = number_format($_POST['receiptionID']);
 
-echo '<script>console.log("'.$_POST['receiptionID'].'");</script>';
-
 $query = "UPDATE receiption_product_service SET is_done = $updatedStatus WHERE rid = $receiptionId";
 $result = pg_query($connect, $query);
+
+echo $result;
 ?>
