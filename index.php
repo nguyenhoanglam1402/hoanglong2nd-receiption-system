35c9028fcfb12af8e937de8fcf61641c91f77a7d
nguyenhoanglam1402 <nguyenhoanglam.emily@gmail.com>
2021-05-14 18:30:02 +0700

    Modified some showing element in Receiption Management Page

--- index.php
+++ index.php
@@ -63,6 +63,13 @@
     </nav>
     </div>
     <main class="page-content">
+      <link rel="stylesheet" href="css/quotes.css">
+      <div class="qoutes-container">
+        <div class="quotes-block">
+          <img class="qoutes-image" src="images/asset/recovery.png" alt="" srcset="">
+          <p class="quote-content">"You have done all of task. Let's take some time for yourself"</p>
+        </div>
+      </div>
     </main>
   </body>
   <script src="js/navigationbar_click.js"></script>
